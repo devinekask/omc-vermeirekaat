b84790313124b48a55bd2889531c9420d8227bb5
vermeirekaat <kaat.vermeire@telenet.be>
2020-10-20 19:38:20 +0200

    experimental files

---
+++ OMC-experiments/01_stars/js/sketch.js
@@ -0,0 +1,25 @@
+// globale variabelen voor de achtergrondkleuren
+let gradientDark;
+let gradientLight;
+
+function setup() {
+    // createCanvas(800, 800); 
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    // kleuren definiëren v/d gradient
+    gradientDark = color(0, 0, 51);
+    gradientLight = color(16, 74, 101);
+    setGradient(gradientDark, gradientLight);
+}
+
+function setGradient(gradientDark, gradientLight) {
+    noFill();
+    for (let i = 0; i < height; i++) {
+        let overflow = map(i, 0, height, 0, 1);
+        let gradient = lerpColor(gradientDark, gradientLight, overflow);
+        stroke(gradient);
+        line(0, i, width, i);
+    }
+}
