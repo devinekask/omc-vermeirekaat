57ef579523e1df2058a6d521807e24efa2bfe853
vermeirekaat <kaat.vermeire@telenet.be>
2020-10-21 21:33:09 +0200

    raindrops fall from the sky

--- OMC-experiments/04_rain/js/sketch.js
+++ OMC-experiments/04_rain/js/sketch.js
@@ -2,6 +2,10 @@
 let gradientDark;
 let gradientLight;
 
+// array voor regendruppels 
+let rain = []; 
+let dropSpeed = 10; 
+
 function setup() {
     // createCanvas(800, 800); 
     createCanvas(windowWidth, windowHeight);
@@ -14,6 +18,11 @@ function draw() {
     setGradient(gradientDark, gradientLight);
     // background(0, 0, 35, 25); 
 
+    for (let i = 0; i < 100; i++) {
+        let raindrop = new Rain;
+        rain.push(raindrop); 
+        rain[i].fall(); 
+    }
 }
 
 function setGradient(gradientDark, gradientLight) {
@@ -23,5 +32,24 @@ function setGradient(gradientDark, gradientLight) {
         let gradient = lerpColor(gradientDark, gradientLight, overflow);
         stroke(gradient);
         line(0, i, width, i);
+    }
+}
+
+class Rain {
+    constructor() {
+        this.x = 0; 
+        this.y = random(0, windowHeight / 2); 
+        this.length = random(50); 
+    }
+    fall() {
+        // frameRate(60); 
+        stroke(255);
+        line(this.x, this.y, this.x, this.y + this.length); 
+        if (this.y < height) {
+            this.y += dropSpeed; 
+        } else {
+            this.x = round(random(width));
+            this.y = -random(random(height));
+        }
     }
 }
