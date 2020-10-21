da411fe04b5da26f509f2e69aafc44958084290d
vermeirekaat <kaat.vermeire@telenet.be>
2020-10-21 21:13:39 +0200

    new experiment for raindrops

---
+++ OMC-experiments/04_rain/js/sketch.js
@@ -0,0 +1,27 @@
+// globale variabelen voor de achtergrondkleuren
+let gradientDark;
+let gradientLight;
+
+function setup() {
+    // createCanvas(800, 800); 
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    // kleuren definiëren v/d gradient
+    gradientDark = color(0, 0, 51);
+    gradientLight = color(16, 74, 101);
+    setGradient(gradientDark, gradientLight);
+    // background(0, 0, 35, 25); 
+
+}
+
+function setGradient(gradientDark, gradientLight) {
+    noFill();
+    for (let i = 0; i < height; i++) {
+        let overflow = map(i, 0, height, 0, 1);
+        let gradient = lerpColor(gradientDark, gradientLight, overflow);
+        stroke(gradient);
+        line(0, i, width, i);
+    }
+}
