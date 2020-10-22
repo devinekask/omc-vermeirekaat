29624fc47b19cbceca0d52e1e6933959cc35eef7
vermeirekaat <kaat.vermeire@telenet.be>
2020-10-22 12:06:20 +0200

    loading api with json

--- OMC-experiments/05_api/js/sketch.js
+++ OMC-experiments/05_api/js/sketch.js
@@ -2,9 +2,26 @@
 let gradientDark;
 let gradientLight;
 
+// weather api 
+let weather; 
+let api = 'http://api.openweathermap.org/data/2.5/weather?q='; 
+let city = 'London'; 
+let apiKey = '&APPID=246867b1139783d7f7a205eae651394b'; 
+let units = '&units=metric'; 
+
 function setup() {
     // createCanvas(800, 800); 
     createCanvas(windowWidth, windowHeight);
+
+    // correcte url opstellen 
+    let url = api + city + apiKey + units; 
+    // json inladen 
+    loadJSON(url, gotData); 
+}
+
+function gotData(data) {
+    weather = data; 
+
 }
 
 function draw() {
