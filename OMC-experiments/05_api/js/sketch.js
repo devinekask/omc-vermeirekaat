5ec4e263a4940f18b30373e5fd3c166afec1dccf
vermeirekaat <kaat.vermeire@telenet.be>
2020-10-22 12:41:08 +0200

    read value of submit form

--- OMC-experiments/05_api/js/sketch.js
+++ OMC-experiments/05_api/js/sketch.js
@@ -5,16 +5,36 @@ let gradientLight;
 // weather api 
 let weather; 
 let api = 'http://api.openweathermap.org/data/2.5/weather?q='; 
-let city = 'London'; 
+// let city = 'London'; 
 let apiKey = '&APPID=246867b1139783d7f7a205eae651394b'; 
-let units = '&units=metric'; 
+let units = '&units=metric';
+
+let inputValue;
 
 function setup() {
-    // createCanvas(800, 800); 
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 800); 
+    // createCanvas(windowWidth, windowHeight);
 
+    // let button = select("submit"); 
+    // button.mousePressed(weatherAsk);
+    const $form = document.querySelector(`.field`);
+    $form.addEventListener(`submit`, handleSubmit);
+
+    input = select("city");
+    
+}
+
+function handleSubmit(e) {
+    e.preventDefault(); 
+    const $inputField = document.querySelector(`.input`); 
+    inputValue = $inputField.value; 
+    console.log(inputValue);
+
+    weatherAsk(); 
+}
+function weatherAsk() {
     // correcte url opstellen 
-    let url = api + city + apiKey + units; 
+    let url = api + input.value() + apiKey + units;
     // json inladen 
     loadJSON(url, gotData); 
 }
